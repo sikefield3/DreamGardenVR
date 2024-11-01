27accbf9316fd20b684e62aa7884da8f0cc93411
sikefield3 <mel22624@mail.de>
2024-11-01 15:21:02 +0100

    create class IrrlichtAdapter

---
+++ src/Adapters/GameEngineAdapter/IrrlichtAdapter/IrrlichtAdapter.h
@@ -0,0 +1,18 @@
+#include <Adapters/GameEngineAdapter/AbstractGameEngineAdapter.h>
+
+class IrrlichtAdapter : public AbstractGameEngineAdapter
+{
+private:
+	/* data */
+public:
+	IrrlichtAdapter(/* args */);
+	~IrrlichtAdapter();
+};
+
+IrrlichtAdapter::IrrlichtAdapter(/* args */)
+{
+}
+
+IrrlichtAdapter::~IrrlichtAdapter()
+{
+}
